1baa12b151b6f38a57f0e7e7db78a6754e8061aa
khalifa <khalifa.hassen.pro@gmail.com>
2026-04-15 13:26:57 +0200

    fix

--- internal/http/middleware/security.go
+++ internal/http/middleware/security.go
@@ -38,7 +38,6 @@ func SecurityHeaders() gin.HandlerFunc {
 		c.Writer.Header().Set("X-Frame-Options", "DENY")
 		c.Writer.Header().Set("X-XSS-Protection", "1; mode=block")
 		c.Writer.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
-		c.Writer.Header().Set("Content-Security-Policy", "default-src 'self'")
 		c.Next()
 	}
 }
